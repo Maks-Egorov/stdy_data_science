57a1dddf298853ae451cf13a56d17a1b1d51f0bd
Maks-Egorov <harry.selldon@gmail.com>
2023-09-18 00:03:30 -0700

    edit game_v2.py

--- project_0/game_v2.py
+++ project_0/game_v2.py
@@ -1,8 +1,13 @@
 import numpy as np
 
 def random_predict(number:int=1) -> int:
-    """Рандомно угадываем число
-
+    """Сначала устанавливаем любое random число, а потом уменьшаем
+    дапазон random чисел, если загаданное число было больше,
+    то диапазон начинается с random числа и до 100, если число снова
+    не угадали, то диапазон random чисел снова сужаем. 
+    Таким же образом в обратную сторону, если загаданное число меньше
+    random числа
+     
     Args:
         number (int, optional): Загаданное число. Defaults to 1.
 
@@ -10,13 +15,21 @@ def random_predict(number:int=1) -> int:
         int: Число попыток
     """
 
-    count = 0
+    count = 0 # счетчик
+    start_num = 1 # начало цикла
+    finish_num = 101 # конец цикла
 
     while True:
         count += 1
-        predict_number = np.random.randint(1, 101) # предполагаемое число
+        predict_number = np.random.randint(start_num, finish_num) # предполагаемое число
         if number == predict_number:
             break # выход из цикла, если угадали
+        else:
+            if number > predict_number:
+                start_num = predict_number 
+            else:
+                finish_num = predict_number
+
     return(count)
 
 def score_game(random_predict) -> int:
